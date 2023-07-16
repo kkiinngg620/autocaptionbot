7054c34a2e21421cedab04e490365e7ee4b76ac7
kkiinngg620 <133698348+kkiinngg620@users.noreply.github.com>
2023-07-16 12:20:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6346310708:AAHGxZI4lFhoOz4ehKA1t365JxLkh7PBkJ8")
-      API_ID = int(os.environ.get("APP_ID", "27157998"))
-      API_HASH = os.environ.get("API_HASH", "45d09c93e37c9b93b6535949c898f906")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+      API_ID = int(os.environ.get("APP_ID", 12345))
+      API_HASH = os.environ.get("API_HASH")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "mr_chaudhary620")
-      ADMIN_ID = int(os.environ.get("ADMIN_ID", "1452198353")) 
-      DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://BOOMERMAN:BOOMERMAN@cluster0.7iadxlk.mongodb.net/?retryWrites=true&w=majority")
+      ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
+      DB_URL = os.environ.get("DATABASE_URL", "")
